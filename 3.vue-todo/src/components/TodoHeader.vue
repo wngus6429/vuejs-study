88c846ba8de017f1bc9f7c6b1c066a290913ed82
ParkJuhyun <wngus6429@gmail.com>
2021-09-09 01:34:20 +0900

    update 9/8

---
+++ 3.vue-todo/src/components/TodoHeader.vue
@@ -0,0 +1,13 @@
+<template>
+  <header><h1>Todo it!</h1></header>
+</template>
+
+<style scoped>
+/* scoped는 이 컴포넌트에서만 사용 */
+h1 {
+  color: #2f3b52;
+  font-weight: 900;
+  margin: 2.5rem 0 1.5rem;
+}
+/* rem = 폰트의 크기에 따라서 배율 조절 */
+</style>
---
+++ 3.vue-todo/src/components/TodoInput.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <input type="text" v-model="newTodoItem" />
+    <button v-on:click="addTodo">add</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data: function() {
+    return {
+      newTodoItem: "",
+    };
+  },
+  methods: {
+    addTodo: function() {
+      console.log(this.newTodoItem);
+      //저장하는 로직
+      localStorage.setItem("으악", this.newTodoItem); //mdn 검색, key와 value
+      this.newTodoItem = ""; //초기화 비워줌
+    },
+  },
+};
+</script>
+
+<style></style>
