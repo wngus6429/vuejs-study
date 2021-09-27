bd19ffb5dfad90f9f60b795030c9e659db879f12
wngus6429 <55697824+wngus6429@users.noreply.github.com>
2021-09-28 02:28:43 +0900

    update

--- 6.vuereal/vue-news/src/views/AskView.vue
+++ 6.vuereal/vue-news/src/views/AskView.vue
@@ -1,9 +1,22 @@
 <template>
-  <main>AskView</main>
+  <div>
+    <div v-for="user in users">{{ user.title }}</div>
+  </div>
 </template>
 
 <script>
-export default {};
-</script>
+import axios from "axios";
 
-<style></style>
+export default {
+  data() {
+    return {
+      user: [],
+    };
+  },
+  async fetch() {
+    const res = await axios.get("https://api.hnpwa.com/v0/show/1.json");
+    console.log(res);
+    this.user = res.data;
+  },
+};
+</script>
--- 6.vuereal/vue-news/src/views/NewsView.vue
+++ 6.vuereal/vue-news/src/views/NewsView.vue
@@ -1,9 +1,27 @@
 <template>
-  <main>NewsView</main>
+  <main>
+    <div v-for="user in users">{{ user.title }}</div>
+  </main>
 </template>
 
 <script>
-export default {};
+import axios from "axios";
+export default {
+  components: {},
+  data() {
+    return {
+      users: [],
+    };
+  },
+  created() {
+    //axios는 프로미스 기반이다.
+    //화살표 안쓰면 let vm = this; 를 해야함
+    axios
+      .get("https://api.hnpwa.com/v0/news/1.json")
+      .then((response) => (this.users = response.data))
+      .catch();
+  },
+};
 </script>
 
 <style></style>
