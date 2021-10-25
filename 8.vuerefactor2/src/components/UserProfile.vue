862dddf857c37fd017bd6b86364587e85c9daba4
wngus6429 <55697824+wngus6429@users.noreply.github.com>
2021-10-26 00:33:37 +0900

    update

---
+++ 8.vuerefactor2/src/components/UserProfile.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="user-container">
+    <div>
+      <i class="far fa-user"></i>
+    </div>
+    <div class="user-description">
+      <div>
+        <!-- {{ userInfo.id }} -->
+        {{ info.id }}
+      </div>
+      <div class="time">
+        <!-- {{ userInfo.created }} -->
+        {{ info.created }}
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    info: Object,
+  },
+  // computed: {
+  //   userInfo() {
+  //     return this.$store.state.user;
+  //   },
+  // },
+};
+</script>
+
+<style scoped>
+.user-container {
+  display: flex;
+  align-items: center;
+  padding: 0.5rem;
+}
+.fa-user {
+  font-size: 2.5rem;
+}
+.user-description {
+  padding-left: 8px;
+}
+.time {
+  font-size: 0.7rem;
+}
+</style>
--- 8.vuerefactor2/src/views/UserView.vue
+++ 8.vuerefactor2/src/views/UserView.vue
@@ -1,18 +1,20 @@
 <template>
   <main>
-    <!-- <p>name : {{ this.$store.state.user.id }}</p>
-    <p>karma : {{ this.$store.state.user.karma }}</p>
-    <p>created : {{ this.$store.state.user.created }}</p> -->
-    <p>name : {{ userInfo.id }}</p>
+    <!-- <UserProfile /> -->
+    <UserProfile :info="userInfo" />
+    <!-- <p>name : {{ userInfo.id }}</p>
     <p>karma : {{ userInfo.karma }}</p>
-    <p>created : {{ userInfo.created }}</p>
+    <p>created : {{ userInfo.created }}</p> -->
   </main>
 </template>
 
 <script>
-// import { fetchUserInfo } from "../api/index";
+import UserProfile from "../components/UserProfile.vue";
 
 export default {
+  components: {
+    UserProfile,
+  },
   computed: {
     userInfo() {
       return this.$store.state.user;
@@ -24,15 +26,6 @@ export default {
     this.$store.dispatch("FETCH_USER", userName); //한개만 넘겨야해서 복수면 객체로
   },
 };
-// Store 사용 안 했을 경우
-// import axios from "axios";
-// export default {
-//   created() {
-//     console.log(this.$route.params);
-//     const userName = this.$route.params.id;
-//     axios.get(`https://api.hnpwa.com/v0/user/${userName}.json`);
-//   },
-// };
 </script>
 
 <style></style>
