99d7448f8bbf67b2c5681b2f98552a261af33dc5
ParkJuhyun <wngus6429@gmail.com>
2021-12-22 01:03:42 +0900

    update 12/21

---
+++ 15.controlled/src/components/CheckBox.vue
@@ -0,0 +1,17 @@
+<template>
+  <input type="checkbox" :value="value" @click="toggleCheckBox" />
+  <!-- v-model:value 는 :value 와 같다 -->
+</template>
+
+<script>
+export default {
+  // @input 이벤트
+  // :value 값
+  props: ["value"], //v-model="checked" , 기본 false
+  methods: {
+    toggleCheckBox() {
+      this.$emit("input", !this.value);
+    },
+  },
+};
+</script>
