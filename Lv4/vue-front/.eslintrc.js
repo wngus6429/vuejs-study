0878957e36b34ab8d746feb8699bf8818a292880
wngus6429 <55697824+wngus6429@users.noreply.github.com>
2021-12-29 15:27:43 +0900

    update 12/29

---
+++ Lv4/vue-front/.eslintrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true,
+  },
+  extends: ["plugin:vue/essential", "eslint:recommended", "@vue/prettier"],
+  parserOptions: {
+    parser: "babel-eslint",
+  },
+  rules: {
+    // production 일떄는 콘솔쓰지 말라고 해둔거
+    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
+    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+  },
+  overrides: [
+    {
+      files: ["**/__tests__/*.{j,t}s?(x)", "**/tests/unit/**/*.spec.{j,t}s?(x)"],
+      env: {
+        jest: true,
+      },
+    },
+  ],
+};
---
+++ Lv4/vue-front/vue.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  devServer: {
+    overlay: false,
+  },
+};
+// 에러 있어도 화면 조작 할 수 있게끔
