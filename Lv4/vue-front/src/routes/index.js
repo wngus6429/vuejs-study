cfdd5375cc73f40dd8afd18917b9eba5e8a74353
wngus6429 <55697824+wngus6429@users.noreply.github.com>
2022-01-06 00:18:21 +0900

    update 1/5

--- Lv4/vue-front/src/routes/index.js
+++ Lv4/vue-front/src/routes/index.js
@@ -1,14 +1,32 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
-import LoginPage from "@/views/LoginPage.vue";
-import SignupPage from "@/views/SignupPage.vue";
+// import LoginPage from "@/views/LoginPage.vue";
+// import SignupPage from "@/views/SignupPage.vue";
 
 Vue.use(VueRouter);
 
 export default new VueRouter({
   mode: "history",
   routes: [
-    { path: "/login", component: LoginPage },
-    { path: "/signup", component: SignupPage },
+    {
+      path: "/",
+      redirect: "/login",
+      // / 에 접속하면 자동으로 /login으로 간다.
+    },
+    {
+      path: "/login",
+      component: () => import("@/views/LoginPage.vue"),
+      // component: LoginPage
+    },
+    {
+      path: "/signup",
+      component: () => import("@/views/SignupPage.vue"),
+      // component: SignupPage
+    },
+    {
+      // 위쪽을 제외한 모든
+      path: "*",
+      component: () => import("@/views/NotFoundPage.vue"),
+    },
   ],
 });
---
+++ Lv4/vue-front/src/views/NotFoundPage.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>Page is Not Found</div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
