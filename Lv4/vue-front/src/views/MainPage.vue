ea5fdbc3f0e3a0ccf810dedbd1cdfa0e63594657
wngus6429 <55697824+wngus6429@users.noreply.github.com>
2022-01-14 02:15:00 +0900

    update 0113

--- Lv4/vue-front/src/views/MainPage.vue
+++ Lv4/vue-front/src/views/MainPage.vue
@@ -2,12 +2,54 @@
   <div>
     <div class="main list-container contents">
       <h1 class="page-header">Today I Learned</h1>
+      <LoadingSpinner v-if="isLoading" />
+      <ul v-else>
+        <PostListItem v-for="postItem in postItems" :key="postItem._id" :postItem="postItem" />
+      </ul>
+      <!-- 스타일링은 common/css에 있다. -->
+      <!-- <li v-for="postItem in postItems" v-bind:key="postItem._id">
+          <div class="post-title">
+            {{ postItem.title }}
+          </div>
+          <div class="post-content">
+            {{ postItem.contents }}
+          </div>
+          <div class="post-time">
+            {{ postItem.createdAt }}
+          </div>
+        </li> -->
     </div>
   </div>
 </template>
 
 <script>
-export default {};
+import PostListItem from "../components/posts/PostListItem.vue";
+import LoadingSpinner from "@/components/common/LoadingSpinner.vue";
+import { fetchPosts } from "@/api/index";
+export default {
+  components: {
+    PostListItem,
+    LoadingSpinner,
+  },
+  data() {
+    return {
+      postItems: [],
+      isLoading: false,
+    };
+  },
+  methods: {
+    async fetchData() {
+      this.isLoading = true;
+      const { data } = await fetchPosts();
+      console.log(data.posts);
+      this.isLoading = false;
+      this.postItems = data.posts;
+    },
+  },
+  created() {
+    this.fetchData();
+  },
+};
 </script>
 
 <style></style>
